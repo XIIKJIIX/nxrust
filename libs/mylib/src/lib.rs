d51a09d08a36ab28bed8996386aa9a68bda782f3
Weeruhputt Supsohmboon <xiikjiix321@gmail.com>
2023-07-06 15:45:01 +0700

    feat(mylib): add add func

--- libs/mylib/src/lib.rs
+++ libs/mylib/src/lib.rs
@@ -1,7 +1,19 @@
+pub mod inner {
+    pub fn add(a: i32, b: i32) -> i32 {
+        a + b
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn it_works2() {
         let result = 2 + 2;
         assert_eq!(result, 4);
     }
